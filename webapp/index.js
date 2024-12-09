ace601c0a3393264b77cebb734f8a08dd1e1eadf
joshiga70 <52832334+joshiga70@users.noreply.github.com>
2024-12-09 21:36:46 +0530

    Update index.js

--- webapp/index.js
+++ webapp/index.js
@@ -20,6 +20,6 @@ app.get('/pod-status', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://<external-ip>:${PORT}`);
 });

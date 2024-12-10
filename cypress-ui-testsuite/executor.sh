c3e6d7bfb132fced2efb6f69be4fe176d7c12737
joshiga70 <joshigirija70@gmail.com>
2024-12-10 23:26:57 +0530

    updated cypress.jon value updation part

--- cypress-ui-testsuite/executor.sh
+++ cypress-ui-testsuite/executor.sh
@@ -3,8 +3,8 @@ BROWSER="electron"
 #npm install
 #sleep 2
 
-#INTGR_SUIT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-#cd $INTGR_SUIT_DIR
+INTGR_SUIT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $INTGR_SUIT_DIR
 echo ">> Running Monitoring.feature file >>"
 npm run test -- -e --browser ${BROWSER} --spec "cypress/integration/BDD/Monitoring.feature"
 sleep 20
